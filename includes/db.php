4e19e72fcdf8249e1aa1a731c2aee12d3f29e4a6
worladev <confidenceworlasi@gmail.com>
2026-06-18 22:32:41 +0000

    modified db port number

--- includes/db.php
+++ includes/db.php
@@ -10,11 +10,12 @@
 $DB_HOST = "127.0.0.1";
 $DB_NAME = "intl_students_db";
 $DB_USER = "root";
+$DB_PORT = "3306";
 $DB_PASS = "";        // default XAMPP password is blank
 
 try {
     $pdo = new PDO(
-        "mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8mb4",
+        "mysql:host={$DB_HOST};dbname={$DB_NAME};port=3306;charset=utf8mb4",
         $DB_USER,
         $DB_PASS,
         [
